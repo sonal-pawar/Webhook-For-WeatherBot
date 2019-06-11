af3a9e8c9e564a42389af906f6b5a6b99f5bde80
sonal-pawar <47026815+sonal-pawar@users.noreply.github.com>
2019-06-11 13:02:14 +0530

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -28,57 +28,21 @@ def webhook():
     return r
 
 def makeResponse(req):
+    result = req.get("result")
+    parameters = result.get("parameters")
     city = parameters.get("geo-city")
     date = parameters.get("date")
     api_key = "8b316cbdc31b46e8ea8aca2e73ed3377"
     base_url = "http://api.openweathermap.org/data/2.5/weather?"
     complete_url = base_url + "appid=" + api_key + "&q=" + city 
-
-    r = requests.get(complete_url) 
+    r = requests.get(complete_url)
     json_object  = r.json()
-    if json_object["cod"] != "404": 
-
-        # store the value of "main" 
-        # key in variable y 
-        y = json_object["main"] 
-
-        # store the value corresponding 
-        # to the "temp" key of y 
-        current_temperature = y["temp"] 
-
-        # store the value corresponding 
-        # to the "pressure" key of y 
-        current_pressure = y["pressure"] 
-
-        # store the value corresponding 
-        # to the "humidity" key of y 
-        current_humidiy = y["humidity"] 
-
-        # store the value of "weather" 
-        # key in variable z 
-        z = json_object["weather"] 
-
-        # store the value corresponding 
-        # to the "description" key at 
-        # the 0th index of z 
-        weather_description = z[0]["description"] 
-
-        # print following values 
-        print(" Temperature (in kelvin unit) = " +
-                        str(current_temperature) +
-            "\n atmospheric pressure (in hPa unit) = " +
-                        str(current_pressure) +
-            "\n humidity (in percentage) = " +
-                        str(current_humidiy) +
-            "\n description = " +
-                        str(weather_description)
-                        ) 
-        description = " Temperature (in kelvin unit) = " + str(current_temperature) + "\n atmospheric pressure (in hPa unit) = " + str(current_pressure) + "\n humidity (in percentage) = " +  str(current_humidiy) +"\n description = " + str(weather_description)
-                    
-    else: 
-        print(" City Not Found ") 
-
-    speech = "The forcast for "+city+ " for "+date+" is  " + description
+    weather = json_object['list']
+    for i in  range(0,30):
+        if date in weather[i]['dt_txt']:
+            condition = weather[i]['weather'][0]['description']
+            break
+    speech = "The forcast for "+city+ " for "+date+" is  " + condition
 
     return {
         "speech": speech,
@@ -96,5 +60,5 @@ def makeResponse(req):
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
-    print("starting app on port {} port".format(port))
+    print("starting app on port %d port", port)
     app.run(debug=False, port=port, host='0.0.0.0')
